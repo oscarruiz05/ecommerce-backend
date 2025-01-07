01627224105aaaf4bdfc1698c0f938339b2bb5b6
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-01-07 01:46:35 -0500

    feat: :sparkles: cors products

--- src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/ProductController.java
+++ src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/admin/products")
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
     public final ProductService productService;
 
