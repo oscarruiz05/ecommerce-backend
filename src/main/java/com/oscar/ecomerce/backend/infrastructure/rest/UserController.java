6bd2b1333300acca550a5b9ae38107d8ae6be0bc
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-02-08 02:40:19 -0500

    feat: :sparkles: cors usuarios

--- src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/UserController.java
+++ src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
     private final UserService userService;
 
