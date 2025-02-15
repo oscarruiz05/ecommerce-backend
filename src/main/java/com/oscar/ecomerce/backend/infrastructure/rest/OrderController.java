0baebadeb5e8a1b92b51106f10ec6ae1c57dd40a
Oscar Ruiz <oscarruiz2614@gmail.com>
2025-02-15 01:48:43 -0500

    feat: :sparkles: cors orden

--- src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/OrderController.java
+++ src/main/java/com/oscar/ecomerce/backend/infrastructure/rest/OrderController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/orders")
 @Slf4j
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class OrderController {
     private final OrderService orderService;
 
